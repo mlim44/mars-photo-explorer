c74489ebf6858bba554cee96b5233c4ab922d599
Maria Phoebe Lim <mlim44@academic.rrc.ca>
2024-10-27 01:27:27 -0500

    Updated the fetchPhotos function to take in date and api key.

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 import { secretValue } from "./external.js";
 
-// scriptconsole.log(secretValue); 
-
-// !! NEEDS TO BE REWORKED TO TAKE IN DATE AND API KEY 
 console.log("Script started.")
-const marsRoverEndpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&earth_date=2015-6-3";
 
-async function fetchPhotos(endpoint) {
+const photosNode = document.querySelector("#photos-container");
+
+async function fetchPhotos(dateString) {
 
     console.log("fetchPhotos started.")
 
+    // see works-consulted 
+    const marsRoverEndpoint = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=${secretValue}` +
+                              `&earth_date=${dateString}`;
+
+    console.log(marsRoverEndpoint)
     try {
-        const response = await fetch(endpoint);
+        const response = await fetch(marsRoverEndpoint);
 
         // boolean for response code in 200s rang
         if(!response.ok) {
             // new keyword means an object is being instantiated
             throw new Error(`Http error: ${response.status}`);
-        }
+        };
 
-        console.log(response)
+        console.log(response);
 
         // parse the response object into a JavaScript Object
-        return response.json();
+        const photoObject = await response.json();
+
+        // deconstructs the json into an object 
+        const { photos: [photo1, photo2, photo3] } = photoObject;
+        // see works-consulted 
+        return [photo1, photo2, photo3].map(photo => {
+            const { 
+                id,
+                camera: { full_name }, 
+                earth_date, 
+                img_src, 
+            } = photo;
+            return { id, full_name, earth_date, img_src };
+        });
     } catch (error) {
         console.error(error.message);
     }
 }
 
-async function displayPhotos() {
+async function loadInitialPhotos() {
 
     const response = await fetchPhotos(marsRoverEndpoint);
     // see works-consulted 
@@ -56,11 +72,15 @@ async function displayPhotos() {
         image.src = img_src
         image.alt = description;
         newLiNode.textContent = description;
-        const photosNode = document.querySelector("#photos");
+        const photosNode = document.querySelector("#photos-container");
         photosNode.appendChild(image);
         photosNode.appendChild(newLiNode);
     });
     
 }
 
-displayPhotos();
+loadInitialPhotos();
+
+// async function displayPhotos(photos, description) {
+
+// }
